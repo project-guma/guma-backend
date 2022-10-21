4ef36b7f610679a2b0aa1f3795d0ff815af8bce3
JooYoung2274 <jooooo2274@gmail.com>
2022-10-21 09:16:01 +0900

    [add] script setting & @nest/swagger setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('QMS API')
+        .setDescription('QMS API Document 입니다. JWT 토큰은 임의로 발급받은 후 우측에 있는 Authorize 에 등록해주세요.')
+        .setVersion('1.0')
+
+        .addBearerAuth(
+            {
+                type: 'http',
+                scheme: 'bearer',
+                name: 'JWT',
+                in: 'header',
+            },
+            'access-token',
+        )
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3000);
 }
 bootstrap();
