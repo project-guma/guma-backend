fec80a9a5ecd4395895c693c8308d5bf4d35f518
JooYoung2274 <jooooo2274@gmail.com>
2022-10-21 18:33:57 +0900

    [add] typeorm & dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { Oauth } from './entities/oauth';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        AuthModule,
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    type: 'mysql',
+                    host: 'localhost',
+                    port: 3306,
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_DATABASE'),
+                    entities: [Oauth],
+                    autoLoadEntities: true,
+                    charset: 'utf8mb4',
+                    synchronize: true,
+                    logging: true, // query 날리는것 로깅
+                    // keepConnectionAlive: true, //hot reloading 할때 필요
+                };
+            },
+        }),
+    ],
+
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
