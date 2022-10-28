5213b548e624d052d269acbf1732e2ec58ded9f4
JooYoung2274 <jooooo2274@gmail.com>
2022-10-28 16:47:04 +0900

    [add] naver open api

--- src/common/utils/naver.api.ts
+++ src/common/utils/naver.api.ts
@@ -1,18 +1,32 @@
 import axios from 'axios';
 
-let config = {
-    method: 'get',
-    url: 'https://openapi.naver.com/v1/search/shop.json?query=두루마리휴지3겹&sort=asc',
-    headers: {
-        'X-Naver-Client-Id': 'AqZZq30_WVGpShV_7Oii',
-        'X-Naver-Client-Secret': 'OQ6wDs66HQ',
-    },
-};
+// 하루에 한 번
+// 오늘 날짜에 맞는 데이터가 Alarm entity 에 있는지 확인
+// 없으면 끝
+// 있으면 해당 컬럼의 SubscribeListId -> ItemId -> name 을 찾아서
+// naver api 로 데이터 긁어오고
+// 긁어온 데이터 Items 테이블에 업데이트 시킴.
 
-axios(config)
-    .then(function (response) {
-        console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+// naver api
+
+export async function naverApi(query: string) {
+    let config = {
+        method: 'get',
+        url: `https://openapi.naver.com/v1/search/shop.json?query=${query}&sort=asc`,
+        headers: {
+            'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
+            'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET,
+        },
+    };
+
+    axios(config)
+        .then(function (response) {
+            const result = JSON.stringify(response.data);
+            console.log(result);
+            return result;
+        })
+        .catch(function (error) {
+            console.log(error);
+            throw error;
+        });
+}
