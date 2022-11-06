dbe634cf1ed283aa56d1570afb9e78be3895f1b8
JooYoung2274 <jooooo2274@gmail.com>
2022-11-06 20:48:11 +0900

    [refac] subscribe.repository provider 추가

--- src/subscribe/subscribe.module.ts
+++ src/subscribe/subscribe.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../entities/user';
 import { SubscribeList } from '../entities/subscribeList';
 import { Alarm } from '../entities/alarm';
+import { SubscribeRepository } from './subscribe.repository';
 
 @Module({
     imports: [TypeOrmModule.forFeature([SubscribeList, Alarm])],
-    providers: [SubscribeService],
+    providers: [SubscribeService, SubscribeRepository],
     controllers: [SubscribeController],
 })
 export class SubscribeModule {}
