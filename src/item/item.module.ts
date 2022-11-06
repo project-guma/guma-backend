e87916348dda86ff75cce2281403f41a85ae104b
JooYoung2274 <jooooo2274@gmail.com>
2022-11-06 21:00:48 +0900

    [refac] item.repository provider 추가

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -3,10 +3,11 @@ import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Items } from '../entities/items';
+import { ItemRepository } from './item.repository';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Items])],
     controllers: [ItemController],
-    providers: [ItemService],
+    providers: [ItemService, ItemRepository],
 })
 export class ItemModule {}
